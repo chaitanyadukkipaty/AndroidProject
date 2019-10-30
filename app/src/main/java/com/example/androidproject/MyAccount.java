4562768c7b340b379707edb39070cd307d25a2c6
Chaitanyakrishna <chaitanyadukkipaty@gmail.com>
2019-10-31 01:04:25 +0530

    Small fixes

--- app/src/main/java/com/example/androidproject/MyAccount.java
+++ app/src/main/java/com/example/androidproject/MyAccount.java
@@ -18,7 +18,10 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,17 +30,23 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class MyAccount extends AppCompatActivity {
     private TextView acc_name;
     private TextView acc_email;
     private TextView acc_phone;
-    //Toolbar toolbar;
+    Toolbar toolbar;
     Context context;
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
     RescuedAdapter rescuedAdapter;
     private String email;
     private String url = "http://192.168.43.77:8081/";
+    private String imgurl;
+    //private String email;
+    private CircleImageView circleImageView;
+    private TextView textView;
     ArrayList<Rescue> list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,14 +59,23 @@ public class MyAccount extends AppCompatActivity {
         acc_email = (TextView) findViewById(R.id.account_email);
         acc_name = (TextView) findViewById(R.id.account_name);
         acc_phone = (TextView) findViewById(R.id.account_number);
+        circleImageView = (CircleImageView) findViewById(R.id.profileImg);
+        textView = (TextView) findViewById(R.id.paisa_contri);
+        toolbar = findViewById(R.id.toolbar_help);
+        setSupportActionBar(toolbar);
         context = this;
 
 
         acc_email.setText(data.getStringExtra("email_id"));
         acc_name.setText(data.getStringExtra("name"));
         acc_phone.setText(data.getStringExtra("phone"));
+        //email = MainActivity.getEmail();
 
+        imgurl = url+data.getStringExtra("email_id")+"_profilepic.jpg";
 
+        Picasso.with(context)
+                .load(imgurl)
+                .into(circleImageView);
 
         ipaddress ip = new ipaddress();
     }
@@ -65,7 +83,8 @@ public class MyAccount extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("test",list.toString());
+        //Log.d("test",list.toString());
+        getamount();
         recyclerView = findViewById(R.id.rescued);
         recyclerView.hasFixedSize();
         layoutManager = new LinearLayoutManager(this);
@@ -90,7 +109,7 @@ public class MyAccount extends AppCompatActivity {
                         JSONObject adoptdata = response.getJSONObject(i);
                         String ngo_name = adoptdata.getString("found_by_ngo");
                         String status = adoptdata.getString("status");
-                        String img_url = url+ adoptdata.getString("img_name");
+                        String img_url = url+ adoptdata.getString("img_addr");
                         list.add(new Rescue(status,ngo_name, img_url));
                         Log.d("test",list.get(i).getStatus()+list.get(i).getNgoname()+list.get(i).getImg_url());
                         rescuedAdapter.notifyDataSetChanged();
@@ -110,6 +129,34 @@ public class MyAccount extends AppCompatActivity {
 
         };
         requestQueue.add(jsonArrayRequest);
+    }
+
+    public void getamount(){
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String dataurl = url+"user/"+email;
+        Log.d("url",dataurl);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, dataurl, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                //acc_name.setText("YOLO");
+                try {
+                    Integer i = response.getInt("amount_don");
+                    String am = String.valueOf(i);
+                    textView.setText(am);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getBaseContext(),"Data not loaded myaccount",Toast.LENGTH_LONG).show();
+            }
+        }){
+
+        };
+        requestQueue.add(jsonObjectRequest);
     }
     private void linearlist(){
         list = new ArrayList<Rescue>();
--- app/src/main/java/com/example/androidproject/signout_fragment.java
+++ app/src/main/java/com/example/androidproject/signout_fragment.java
@@ -2,6 +2,8 @@ package com.example.androidproject;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,6 +11,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,21 +33,30 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class signout_fragment extends Fragment {
+    public static final int PICK_IMAGE = 1;
     private String url;
     private TextView alreadyaccount;
     private FrameLayout parentframeLayout;
+    private Button uploadimg;
     private Button button;
     private EditText email;
     private EditText name;
     private EditText phone;
     private EditText password;
+    private EditText cpassword;
+    CircleImageView circleImageView;
+    private String imgbase64="p";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -54,10 +67,13 @@ public class signout_fragment extends Fragment {
         parentframeLayout = getActivity().findViewById(R.id.register_framelayout);
 
         button = view.findViewById(R.id.btn_signout);
+        uploadimg = view.findViewById(R.id.uploadimg);
         email = view.findViewById(R.id.signout_email);
         name = view.findViewById(R.id.signout_name);
         phone = view.findViewById(R.id.phone_number);
         password = view.findViewById(R.id.signout_password);
+        cpassword = view.findViewById(R.id.signout_confirmpassword);
+        circleImageView = view.findViewById(R.id.logosignup);
         ipaddress ip = new ipaddress();
         url = ip.getIp();
         url = url+"register";
@@ -76,6 +92,16 @@ public class signout_fragment extends Fragment {
             }
         });
 
+        uploadimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+            }
+        });
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -83,37 +109,48 @@ public class signout_fragment extends Fragment {
                 final String signup_password = password.getText().toString();
                 final String signup_phone = phone.getText().toString();
                 final String signup_name = name.getText().toString();
-                HashMap data = new HashMap();
-                data.put("email_id",signup_email);
-                data.put("password",signup_password);
-                data.put("name",signup_name);
-                data.put("phone",signup_phone);
-                RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(data),
-                        new Response.Listener<JSONObject>() {
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                Intent intent = new Intent(getActivity(),MainActivity.class);
-                                try {
-                                    intent.putExtra("email_id",response.getString("email_id"));
-                                    intent.putExtra("name",response.getString("name"));
-                                    intent.putExtra("phone",response.getString("phone"));
-                                    startActivity(intent);
-                                }catch (JSONException e){
-                                    e.printStackTrace();
-                                }
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getActivity(),"Wrong la Username or Password",Toast.LENGTH_LONG).show();
-                    }
-                }){
+                final String signup_cpassword = cpassword.getText().toString();
 
-                };
-                requestQueue.add(jsonObjectRequest);
+                String status = check(signup_email,signup_password,signup_phone,signup_name,signup_cpassword, imgbase64);
+                if(status == "Pass"){
+                    HashMap data = new HashMap();
+                    data.put("email_id",signup_email);
+                    data.put("password",signup_password);
+                    data.put("name",signup_name);
+                    data.put("phone",signup_phone);
+                    data.put("image",imgbase64);
+                    RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(data),
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    Intent intent = new Intent(getActivity(),MainActivity.class);
+                                    try {
+                                        intent.putExtra("email_id",response.getString("email_id"));
+                                        intent.putExtra("name",response.getString("name"));
+                                        intent.putExtra("phone",response.getString("phone"));
+                                        startActivity(intent);
+                                    }catch (JSONException e){
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getActivity(),"Wrong la Username or Password",Toast.LENGTH_LONG).show();
+                        }
+                    }){
+
+                    };
+                    requestQueue.add(jsonObjectRequest);
+                }
+                else {
+                    Toast.makeText(getActivity(),status,Toast.LENGTH_LONG).show();
+                }
+
             }
         });
+
     }
 
     public void setFragment(Fragment fragment){
@@ -121,5 +158,50 @@ public class signout_fragment extends Fragment {
         fragmentTransaction.setCustomAnimations(R.anim.silde_from_left,R.anim.slide_out_from_right);
         fragmentTransaction.replace(parentframeLayout.getId(),fragment);
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE  && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] b = baos.toByteArray();
+                imgbase64 = Base64.encodeToString(b, Base64.DEFAULT);
+                circleImageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    static boolean isValid(String email) {
+        String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        return email.matches(regex);
+    }
+
+    public String check(String signup_email,String signup_password,String signup_phone,String signup_name,String signup_cpassword, String imgbase64){
+        if(signup_cpassword.isEmpty() || signup_email.isEmpty() || signup_phone.isEmpty() || signup_password.isEmpty() || imgbase64.isEmpty()){
+            return "Fields are empty";
+        }
+        else if(!(signup_password.equals(signup_cpassword))) {
+            return "Password don't match";
+        }
+        else if(! (signup_phone.length()==10 || signup_phone.length()==11 || signup_phone.length()==8)){
+            return "Contact number size is wrong";
+        }
+        else if(! isValid(signup_email)){
+            return "Email Id format is worng";
+        }
+        else{
+            return "Pass";
+        }
     }
 }
